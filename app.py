5b7fb967a2b4ae3691af1eb382e6df8092bb6641
muks888 <67940699+muks888@users.noreply.github.com>
2024-08-21 21:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = WheatModel()
 
 @app.get('/')
 def home():
-    return{"Hello":"Welcome to the RMS first ML app."}
+    return{"Hello":"Welcome to the RMS first ML app mod2."}
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted flower species with the confidence
 @app.post('/predict')
@@ -31,7 +31,7 @@ def predict_species(wheat: WheatSpecies):
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8008)
+    uvicorn.run(app, host='0.0.0.0', port=8005)
     
     ## local host--> 127.0.0.0  
     ## host --> 0.0.0.0 allows all host
